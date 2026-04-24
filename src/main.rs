38c2e2e9894c1d91c6657901b317bc6bc49c88b2
jmik05 <janus.miklavcic@gmial.com>
2026-04-24 13:52:26 +0200

    getterji na torti

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ mod logika;
 mod generiranje_narocil;
 mod display;
 
+use crate::torta::{Torta, Nadstropje, Okus, Preliv, Topping};
+
 fn main() {
-    println!("Hello, World!");
+    let prvo = Nadstropje::novo(Okus::Cokolada, Preliv::Karamelni);
+
+    let drugo = Nadstropje::novo(Okus::Vanilija, Preliv::Sadni);
+
+    let mut torta = Torta::nova(prvo);
+
+    torta.dodaj_nadstropje(drugo);
+
+    torta.dodaj_topping_zadnjemu(Topping::Cesnja);
 }
